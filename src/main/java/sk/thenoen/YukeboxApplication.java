ab3486612be9e0a24b918372ad5c3eb855d8590d
mkordik <kordikmarek@davincisoftware.sk>
2016-12-04 16:41:47 +0100

    initial commit

---
+++ src/main/java/sk/thenoen/YukeboxApplication.java
@@ -0,0 +1,12 @@
+package sk.thenoen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YukeboxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YukeboxApplication.class, args);
+	}
+}
